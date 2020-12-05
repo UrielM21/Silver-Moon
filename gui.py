66b4dc2ea1d5135cb6c8136a03550b90b3800968
Uriel <uriel.a.munnoz@gmail.com>
2020-12-05 13:58:48 -0300

    firstGUI

--- gui.py
+++ gui.py
@@ -1,16 +1,27 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
-from temp import Ui_MainWindow
+from TemplateGUI import Ui_Form
+import qdarkstyle
 
 
-
-
-_, Window = uic.loadUiType("temp.ui")
-Form = Ui_MainWindow
+_, Window = uic.loadUiType("templateGUI.ui")
+Form = Ui_Form
 
 app = QApplication([])
 window = Window()
 form = Form()
 form.setupUi(window)
+
+# setup stylesheet
+app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+# or in new API
+app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
+
 window.show()
+
+form.viewer.loadImageFromFile("./logo_zetes.jpeg")
+form.viewer.show()
+
+
+
 app.exec_()
